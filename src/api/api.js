d9d3526cc479a75b5197c833e2c15b5c9efddd69
NickVendetti <nrvendetti@gmail.com>
2025-02-10 22:48:45 -0500

    .env added

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
-
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "https://jobly-backend-r3mk.onrender.com";
 class JoblyApi {
   static token = localStorage.getItem("token"); // Read token from storage
 
